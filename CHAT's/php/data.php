24320707c2d27ebaaa294b44bd74e26568e63295
swethadurai <111978148+swethadurai@users.noreply.github.com>
2024-04-10 12:52:36 +0530

    Add files via upload

---
+++ CHAT's/php/data.php
@@ -0,0 +1,18 @@
+<?php 
+ while ($row = mysqli_fetch_assoc($sql)){
+  
+    $output.='<a href="chat.php?userid='.$row['uniqueid'].'" style="text-decoration: none;
+    color: #000;"> <div class="row mt-4">
+    <div class="col-md-3">
+        <img src="php/images/'. $row['img'] . '?>" alt="" width="80px" height="80px">
+    </div>
+    <div class="col-md-6">
+        <h5>'.$row['fname']." " .$row['lname'].'</h5>
+       <p>active now</p>
+    </div>
+    <div class="col-md-3">
+        
+    </div>
+</div><hr>';
+}
+?>
